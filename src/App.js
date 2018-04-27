bc2e60bd1a9b846793b7b1733c562b88f3ae8788
Ho Yee Ting <yeeting.ho@gu3.co.jp>
2018-04-27 15:30:14 +0900

    replace \n to <br>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,59 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import './App.css';
 
+
+import TextField from 'material-ui/TextField';
+import Grid from 'material-ui/Grid';
+import Paper from 'material-ui/Paper';
+
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      textValue: null,
+    };
+  }
+
+
+  handleChange(e) {
+    // replace \n to <br>
+    // it seems \n but it is \\n
+    var str = e.target.value.replace(/\\n/g, '\n');
+    this.setState({textValue: str});
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+        <div style={{ padding: 20 }}>
+        <Grid container spacing={40}>
+        <Grid item xs={12}>
+          <Paper>Hello World</Paper>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            multiline={true}
+            rows={10}
+            rowsMax={10}
+            fullWidth={true}
+            label="こちらに貼ってください"
+            onChange={this.handleChange.bind(this)}
+          ></TextField>
+        </Grid>
+        <Grid item xs={12}>
+          <TextField
+            multiline={true}
+            rows={10}
+            rowsMax={10}
+            fullWidth={true}
+            value={this.state.textValue}
+          />
+        </Grid>
+      </Grid>
+      </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
