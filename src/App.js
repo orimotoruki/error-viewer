2184ebb722b7052811b2063051053faccc413bbb
Ho Yee Ting <yeeting.ho@gu3.co.jp>
2018-05-02 18:12:30 +0900

    add settings to change color of row (delete option is not working)

--- src/App.js
+++ src/App.js
@@ -1,36 +1,76 @@
 import React, { Component } from 'react';
 import './App.css';
 
-
 import TextField from 'material-ui/TextField';
 import Grid from 'material-ui/Grid';
-import Paper from 'material-ui/Paper';
 
+import { CookiesProvider, Cookies } from 'react-cookie';
+
+import Header from './Header';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      textValue: null,
+      textValue: [],
+      wordData: {},
     };
-  }
 
+    this.handleCookieChange = this.handleCookieChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+
+    this.state.wordData = this.loadCookies();
+  }
 
   handleChange(e) {
     // replace \n to <br>
     // it seems \n but it is \\n
     var str = e.target.value.replace(/\\n/g, '\n');
-    this.setState({textValue: str});
+    var sentences = str.split("\n");
+    var result = [];
+
+    for (var idx in sentences){
+      var color = '#FFFFFF';
+      var sentence = sentences[idx]
+      for (var key in this.state.wordData){
+        if (sentence.includes(key)){
+          color = this.state.wordData[key];
+          break;
+        }
+      }
+      result.push({color: color, sentence: sentence})
+    }
+    this.setState({textValue: result});
+  }
+
+  loadCookies(){
+    console.log("loadCookies");
+    //reload cookies
+    const cookies = new Cookies();
+    var words = cookies.get("word");
+    if (words === undefined){
+      words = {}
+    }
+    return words;
+  }
+
+  handleCookieChange(e){
+    console.log("handleCookieChange");
+    //reload cookies
+    var words = this.loadCookies();
+    this.setState({wordData: words});
   }
 
   render() {
     return (
+
       <div className="App">
+      <CookiesProvider>
+      <Header handler= {this.handleCookieChange} />
+
+
         <div style={{ padding: 20 }}>
         <Grid container spacing={40}>
-        <Grid item xs={12}>
-          <Paper>Hello World</Paper>
-        </Grid>
         <Grid item xs={12}>
           <TextField
             multiline={true}
@@ -38,24 +78,22 @@ class App extends Component {
             rowsMax={10}
             fullWidth={true}
             label="こちらに貼ってください"
-            onChange={this.handleChange.bind(this)}
+            onChange={this.handleChange}
           ></TextField>
         </Grid>
         <Grid item xs={12}>
-          <TextField
-            multiline={true}
-            rows={10}
-            rowsMax={10}
-            fullWidth={true}
-            value={this.state.textValue}
-          />
+          <div className="message">
+            {this.state.textValue.map((value, index) => <div key={index} style={{backgroundColor: value.color}}>{value.sentence}</div>)}
+          </div>
         </Grid>
       </Grid>
       </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        </CookiesProvider>
       </div>
+
     );
   }
 }
---
+++ src/Header.js
@@ -0,0 +1,267 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import AppBar from "material-ui/AppBar";
+import Toolbar from "material-ui/Toolbar";
+import Typography from "material-ui/Typography";
+import IconButton from "material-ui/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Delete from '@material-ui/icons/Delete';
+
+import Drawer from 'material-ui/Drawer';
+import List from 'material-ui/List';
+import Divider from 'material-ui/Divider';
+import { ListItem, ListItemText } from 'material-ui/List';
+
+import Button from 'material-ui/Button';
+import TextField from 'material-ui/TextField';
+import Dialog, {
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from 'material-ui/Dialog';
+
+import Table, { TableBody, TableCell, TableHead, TableRow } from 'material-ui/Table';
+import Paper from 'material-ui/Paper';
+
+import { CookiesProvider, Cookies } from 'react-cookie';
+
+let id = 0;
+function createData(word, color) {
+  id += 1;
+  return { id, word, color };
+}
+
+
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isDrawerOpen: false,
+      isDialogOpen: false,
+      data: {
+               word: '',
+               color: ''
+             },
+      all_data: [],
+      display_data: [],
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.dialogConfirm = this.dialogConfirm.bind(this);
+
+
+    //set default
+    const cookies = new Cookies();
+    var words = cookies.get("word");
+    if (words === undefined){
+      words = {}
+    }
+    this.state.all_data = words;
+    var display_data = []
+    for (var key in words){
+      display_data.push(createData(key, words[key]));
+    }
+    this.state.display_data = display_data;
+  }
+
+  toggleDrawer = (open) => () => {
+   this.setState({
+     isDrawerOpen: open
+   });
+  };
+
+  toggleDialog = (open) => () => {
+   this.setState({
+     isDialogOpen: open
+   });
+  };
+
+  dialogConfirm(event){
+    // close the dialog
+    this.setState({
+      isDialogOpen: false
+    });
+    // handler of super class
+    this.props.handler(event);
+  }
+
+  handleChange(event) {
+    // ネストされたオブジェクトのdataまでアクセスしておく
+    var data = this.state.data;
+
+    // eventが発火したname属性名ごとに値を処理
+    switch (event.target.name) {
+        case 'word':
+            data.word = event.target.value;
+            break;
+        case 'color':
+            data.color = event.target.value;
+            break;
+        default:
+          break;
+    }
+
+    // 状態を更新
+    this.setState({
+        data: data
+    });
+  };
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    // add cookie
+    var words = this.state.all_data;
+    words[this.state.data.word] = this.state.data.color;
+    this.setState({all_data: words});
+    var display_data = []
+    for (var key in words){
+      display_data.push(createData(key, words[key]));
+    }
+    this.setState({display_data: display_data});
+    const cookies = new Cookies();
+    cookies.set("word",
+                words,
+                { path: '/', maxAge: 2147483647 });
+  };
+
+  deleteCookie = function(word){
+    console.log(word.target);
+    //remove Cookie
+    // var words = this.state.all_data;
+    //
+    // // delete words[this.state.data.word];
+    // this.setState({all_data: words});
+    // var display_data = []
+    // for (var key in words){
+    //   display_data.push(createData(key, words[key]));
+    // }
+    // this.setState({display_data: display_data});
+    // const cookies = new Cookies();
+    // cookies.set("word",
+    //             words,
+    //             { path: '/', maxAge: 2147483647 });
+
+  }
+
+  render() {
+    return (
+      <CookiesProvider>
+        <div className="Drawer">
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="Menu"
+              >
+                <MenuIcon onClick={this.toggleDrawer(true)} />
+              </IconButton>
+              <Typography variant="title" color="inherit">
+                Hello World
+              </Typography>
+            </Toolbar>
+          </AppBar>
+
+          <Drawer
+            open={this.state.isDrawerOpen}
+            onClose={this.toggleDrawer(false)}>
+            <div
+              tabIndex={0}
+              role="button"
+            >
+            <div>
+              <List><ListItem button>
+                <ListItemText primary="Main Page"
+                  onClick={this.toggleDrawer(false)}
+                  onKeyDown={this.toggleDrawer(false)}/>
+              </ListItem></List>
+              <Divider />
+              <List><ListItem button>
+                <ListItemText primary="Settings"
+                  onClick={this.toggleDialog(true)}
+                  onKeyDown={this.toggleDialog(true)}/>
+              </ListItem></List>
+            </div>
+          </div>
+        </Drawer>
+
+        <Dialog
+          open={this.state.isDialogOpen}
+          onClose={this.toggleDialog(false)}
+          aria-labelledby="form-dialog-title"
+        >
+          <DialogTitle id="form-dialog-title">Subscribe</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              To subscribe to this website, please enter your email address here. We will send
+              updates occationally.
+            </DialogContentText>
+
+
+            <Paper>
+              <Table>
+                <TableHead>
+                  <TableRow>
+                    <TableCell>Word</TableCell>
+                    <TableCell>Color</TableCell>
+                    <TableCell></TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {this.state.display_data.map(n => {
+                    return (
+                  <TableRow key={n.id}>
+                    <TableCell>{n.word}</TableCell>
+                    <TableCell>{n.color}</TableCell>
+                    <TableCell>
+                      <Button onClick={this.deleteCookie} word={n.word} index={n.id} variant="raised" color="secondary">
+                        Delete
+                        <Delete/>
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            </Paper>
+
+            <form autoComplete="off"
+              onSubmit={this.handleSubmit}>
+              <TextField
+                required
+                id="word"
+                name="word"
+                label="word"
+                margin="normal"
+                value={this.state.name}
+                onChange={this.handleChange}
+              />
+              <TextField
+                required
+                id="color"
+                name="color"
+                label="color"
+                margin="normal"
+                type="color"
+                value={this.state.name}
+                onChange={this.handleChange}
+              />
+              <Button type="submit" color="primary">追加</Button>
+            </form>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.dialogConfirm} color="primary">
+              確定
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </div>
+      </CookiesProvider>
+    );
+  }
+}
+
+export default Header;
